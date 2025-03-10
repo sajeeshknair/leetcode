e702eee9b84e3f8d1990f428b9532cc9bf4d6f8d
sajeeshknair <sajeeshknair@gmail.com>
2025-03-10 16:54:46 +0530

    Create 1-twosum.cpp

---
+++ Hashing/1-twosum.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/two-sum/description/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i;
+        unordered_map <int, int> hash;
+        
+        for (i = 0; i < nums.size(); i++) {
+            if (hash.find(target - nums[i]) == hash.end()) {
+                hash[nums[i]] = i;
+            } else {
+                return {hash[target - nums[i]], i};
+            }
+        }
+        
+        return {};
+    }
+};
